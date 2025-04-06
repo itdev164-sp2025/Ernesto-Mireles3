9bd7c2126e19773d3af11ce60505229ddf55b8f7
Ernesto Mireles <mireleej@gmatc.matc.edu>
2025-04-06 14:43:58 -0500

    Activity 7

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import { H1 } from '../components/Heading'
 
-const BlogPost = ({ data }) => {
-  const { title, body } = data.contentfulBlogPost
+const BlogPost =({ data }) => {
+    const { title, body } = data.contentfulBlogPost;
 
-  return (
-    <Layout>
-      <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{__html: body.childMarkdownRemark.html}}></div>
-    </Layout>
-  )
+    return (
+        <Layout>
+            <H1>(title)</H1>
+            <div dangerouslySetInnerHTML={{__html: body.childMarkDownRemark.html}}></div>
+        </Layout>
+    );
 }
 
-export default BlogPost
+export default BlogPost;
 
 export const pageQuery = graphql`
-  query blogPostQuery($slug: String!) {
-    contentfulBlogPost(slug: {eq: $slug}) {
-      title
-      slug
-      body {
-        childMarkdownRemark {
-          html
+    
+    query blogPostQuery($slug: String!) {
+      contentfulBlogPost(slug: {eq: $slug}) {
+        title
+        slug
+        body {
+          childMarkdownRemark {
+            html
+          }
         }
       }
-    }
-  }
+}
 `
